70e4ca1399ec1db4ebee338cc0eab886eb030ad8
Yaka <yaka88@gmail.com>
2023-08-17 16:46:58 +0800

    Add project files.

---
+++ frmMain.Designer.cs
@@ -0,0 +1,239 @@
+﻿namespace Goldbach
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtNumber = new System.Windows.Forms.TextBox();
+            this.btnPrime = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtCount = new System.Windows.Forms.TextBox();
+            this.btnEven = new System.Windows.Forms.Button();
+            this.txtEvenCount = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtTime = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnPrimePair = new System.Windows.Forms.Button();
+            this.btnDecide = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.richTextPrime = new System.Windows.Forms.RichTextBox();
+            this.richTextPrimePair = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Even:";
+            // 
+            // txtNumber
+            // 
+            this.txtNumber.Location = new System.Drawing.Point(67, 13);
+            this.txtNumber.Name = "txtNumber";
+            this.txtNumber.Size = new System.Drawing.Size(100, 26);
+            this.txtNumber.TabIndex = 1;
+            this.txtNumber.Text = "1000";
+            this.txtNumber.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // btnPrime
+            // 
+            this.btnPrime.Location = new System.Drawing.Point(170, 10);
+            this.btnPrime.Name = "btnPrime";
+            this.btnPrime.Size = new System.Drawing.Size(140, 30);
+            this.btnPrime.TabIndex = 2;
+            this.btnPrime.Text = "Prime Collection";
+            this.btnPrime.UseVisualStyleBackColor = true;
+            this.btnPrime.Click += new System.EventHandler(this.btnPrime_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(316, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Count:";
+            // 
+            // txtCount
+            // 
+            this.txtCount.Location = new System.Drawing.Point(380, 12);
+            this.txtCount.Name = "txtCount";
+            this.txtCount.ReadOnly = true;
+            this.txtCount.Size = new System.Drawing.Size(90, 26);
+            this.txtCount.TabIndex = 5;
+            this.txtCount.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // btnEven
+            // 
+            this.btnEven.Location = new System.Drawing.Point(1038, 10);
+            this.btnEven.Name = "btnEven";
+            this.btnEven.Size = new System.Drawing.Size(134, 30);
+            this.btnEven.TabIndex = 6;
+            this.btnEven.Text = "Even Collection";
+            this.btnEven.UseVisualStyleBackColor = true;
+            this.btnEven.Click += new System.EventHandler(this.btnEven_Click);
+            // 
+            // txtEvenCount
+            // 
+            this.txtEvenCount.Location = new System.Drawing.Point(922, 13);
+            this.txtEvenCount.Name = "txtEvenCount";
+            this.txtEvenCount.ReadOnly = true;
+            this.txtEvenCount.Size = new System.Drawing.Size(111, 26);
+            this.txtEvenCount.TabIndex = 8;
+            this.txtEvenCount.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(857, 16);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 20);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Result:";
+            // 
+            // txtTime
+            // 
+            this.txtTime.Location = new System.Drawing.Point(558, 13);
+            this.txtTime.Name = "txtTime";
+            this.txtTime.ReadOnly = true;
+            this.txtTime.Size = new System.Drawing.Size(100, 26);
+            this.txtTime.TabIndex = 11;
+            this.txtTime.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(476, 15);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(78, 20);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Time(ms):";
+            // 
+            // btnPrimePair
+            // 
+            this.btnPrimePair.Location = new System.Drawing.Point(664, 11);
+            this.btnPrimePair.Name = "btnPrimePair";
+            this.btnPrimePair.Size = new System.Drawing.Size(109, 30);
+            this.btnPrimePair.TabIndex = 12;
+            this.btnPrimePair.Text = "Prime Pair";
+            this.btnPrimePair.UseVisualStyleBackColor = true;
+            this.btnPrimePair.Click += new System.EventHandler(this.btnPrimePair_Click);
+            // 
+            // btnDecide
+            // 
+            this.btnDecide.Location = new System.Drawing.Point(777, 11);
+            this.btnDecide.Name = "btnDecide";
+            this.btnDecide.Size = new System.Drawing.Size(74, 30);
+            this.btnDecide.TabIndex = 13;
+            this.btnDecide.Text = "Decide";
+            this.btnDecide.UseVisualStyleBackColor = true;
+            this.btnDecide.Click += new System.EventHandler(this.btnDecide_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Location = new System.Drawing.Point(1178, 14);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(85, 24);
+            this.checkBox1.TabIndex = 14;
+            this.checkBox1.Text = "Senary";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // richTextPrime
+            // 
+            this.richTextPrime.Location = new System.Drawing.Point(16, 46);
+            this.richTextPrime.Name = "richTextPrime";
+            this.richTextPrime.ReadOnly = true;
+            this.richTextPrime.Size = new System.Drawing.Size(480, 610);
+            this.richTextPrime.TabIndex = 15;
+            this.richTextPrime.Text = "";
+            this.richTextPrime.WordWrap = false;
+            // 
+            // richTextPrimePair
+            // 
+            this.richTextPrimePair.Location = new System.Drawing.Point(504, 47);
+            this.richTextPrimePair.Name = "richTextPrimePair";
+            this.richTextPrimePair.ReadOnly = true;
+            this.richTextPrimePair.Size = new System.Drawing.Size(742, 610);
+            this.richTextPrimePair.TabIndex = 16;
+            this.richTextPrimePair.Text = "";
+            this.richTextPrimePair.WordWrap = false;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1258, 664);
+            this.Controls.Add(this.richTextPrimePair);
+            this.Controls.Add(this.richTextPrime);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.btnDecide);
+            this.Controls.Add(this.btnPrimePair);
+            this.Controls.Add(this.txtTime);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtEvenCount);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnEven);
+            this.Controls.Add(this.txtCount);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnPrime);
+            this.Controls.Add(this.txtNumber);
+            this.Controls.Add(this.label1);
+            this.Name = "frmMain";
+            this.Text = "Goldbach";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtNumber;
+        private System.Windows.Forms.Button btnPrime;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtCount;
+        private System.Windows.Forms.Button btnEven;
+        private System.Windows.Forms.TextBox txtEvenCount;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtTime;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnPrimePair;
+        private System.Windows.Forms.Button btnDecide;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.RichTextBox richTextPrime;
+        private System.Windows.Forms.RichTextBox richTextPrimePair;
+    }
+}
+
---
+++ frmMain.cs
@@ -0,0 +1,261 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Goldbach
+{
+    public partial class frmMain : Form
+    {
+        bool[] intPrimeCol;
+        int intNoBase = 6;
+        //
+        /*  struct PPData
+          {
+              public int Even;
+              public int PPNumber;
+              public List<int> PrimePair;
+          }*/
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        string ToSenary(int intNumber)   //  to base 6
+        {
+            if (intNumber < intNoBase)
+                return intNumber.ToString("00");
+            StringBuilder strNumber = new StringBuilder("");
+            while (intNumber > 0)
+            {
+                strNumber.Insert(0,intNumber % intNoBase); // 1
+                intNumber /= intNoBase;
+            }
+            return strNumber.ToString();
+        }
+        int ParseSenary(string strNumber)
+        {
+            int intNumber = 0;
+            for(int i = 0; i < strNumber.Length; i++)
+            {
+                intNumber = intNumber * intNoBase + strNumber[i] - 48;
+            }
+            return intNumber;
+        }
+        private void btnPrime_Click(object sender, EventArgs e)
+        {
+            int intEven =  checkBox1.Checked? ParseSenary(txtNumber.Text):int.Parse(txtNumber.Text);
+            int intSqrtEven = (int)Math.Sqrt(intEven);
+            int intCount = 0;
+            richTextPrimePair.Text = "";
+            intPrimeCol = new bool[intEven];
+            //StringBuilder strPrimeCol = new StringBuilder();
+            richTextPrime.Clear();
+            richTextPrime.AppendText(" 01\t");
+            DateTime dtStart = DateTime.Now;
+            for(int i = 3; i < intEven; i=i+2)
+            {
+                if (intPrimeCol[i] == false)  //false means Prime
+                {
+                    intCount++;
+                    if (i <= intSqrtEven)
+                    {
+                        int istep = i << 1;
+                        for (int j = i * i; j < intEven; j = j + istep)//for (int j = i << 1; j < intEven; j = j + i)
+                            intPrimeCol[j] = true;
+                    }
+                }
+
+                if (intCount < byte.MaxValue) //save time
+                {   
+                    if (checkBox1.Checked)
+                    {
+                        richTextPrime.SelectionColor = Color.Black;
+                        richTextPrime.AppendText(ToSenary(i - 1));
+                        richTextPrime.AppendText("\t");
+                    }
+                    if (i % intNoBase == 1)
+                        richTextPrime.AppendText("\n ");
+                    richTextPrime.SelectionColor = intPrimeCol[i] ? Color.Black : Color.Red;
+                    //richTextPrime.AppendText(ToSenary(i));
+                    richTextPrime.AppendText(checkBox1.Checked?ToSenary(i):i.ToString("00"));
+                    richTextPrime.AppendText("\t");
+                }
+            }
+            txtTime.Text = (DateTime.Now - dtStart).TotalMilliseconds.ToString("N4");
+            txtCount.Text = intCount.ToString();
+           // richTextPrime.Text = strPrimeCol.ToString(); 
+
+        }
+
+        private void btnEven_Click(object sender, EventArgs e)
+        {
+
+            int intEven = intPrimeCol.Length;
+            StringBuilder strEvenCol = new StringBuilder();
+            int[] intEvenCol = new int[intEven + 1]; // pp count
+            for (int i = 5; i <= (intEven >> 1); i = i + 2)  //step = 2;//ignore 3
+            {
+                if (intPrimeCol[i] == false)
+                {
+                    for (int j = i; j <= intEven - i; j = j + 2)
+                    {
+                        if (intPrimeCol[j] == false)// && i != j)  相同质数不计算
+                        {
+                            intEvenCol[i + j]++;
+                           //  if(intEvenCol[i + j]==0)
+                           //  intEvenCol[i + j] = i;
+                        }
+                    }
+                }
+            }
+
+            int intLimite = Math.Min(short.MaxValue, intEven); //save time)
+            for (int i = 2; i <= intLimite; i = i + 2)
+            {
+                strEvenCol.Append(intEvenCol[i].ToString("00  "));
+                if (i % 6 == 0)
+                {
+                    strEvenCol.Append("\t");
+                    if (i % 30 == 0)
+                        strEvenCol.Append("\r\n");
+                }
+            }
+            richTextPrimePair.Text = strEvenCol.ToString();
+            DecideExcludePP(ref intEvenCol);
+        }
+        void DecideExcludePP(ref int[] intEvenCol)//例外pp数
+        {
+            int intEven = intPrimeCol.Length;
+            int intBase = 2, intStep = 2, intBaseIndex = 0;// int intBase = 2
+            int intExclude = 0;
+            for (int i = 3; intBase * i <= intEven; i = i + 2)  //step = 2;
+            {
+                if (intPrimeCol[i] == false)
+                {
+                    intStep = intBase;
+                    intBase *= i;
+                    intBaseIndex = intBase;
+                    for (int j = intStep; j < intEven && intBaseIndex <= intEven; j += intStep)
+                    {
+                        if (j < intBaseIndex)
+                        {
+                            if (intEvenCol[j] > intEvenCol[intBaseIndex])
+                            {
+                                intExclude++;
+                            }
+                        }
+                        else
+                        {
+                            intBaseIndex += intBase;
+                        }
+                    }
+
+                }
+            }
+            txtEvenCount.Text = string.Format("Ex {0}", intExclude);
+        }
+
+        bool IsSucceed(int intEven, int intBase)
+        {
+            for (int i = 5; i <= (intEven >> 1); i = i + 2)//pass  1，3
+            {
+                if (intPrimeCol[i] == false && intPrimeCol[intEven - i] == false)
+                {
+                    if(!intPrimeCol[i + intBase])// || !intPrimeCol[intEven - i + intBase])//小质数+6依然为质数
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+        void DecideSucceedPP()   //PP对小质数 + intBase 仍可构成PP对
+        {
+            int intBase = 6; //  2*3*5*7*11
+            
+            for(int i = intBase <<1; i <= intPrimeCol.Length; i += 2)
+            {
+                if(IsSucceed(i, intBase) == false)
+                {
+                    txtEvenCount.Text = string.Format("Ex {0}", i);
+                    return;
+                }
+            }
+            txtEvenCount.Text = string.Format("Y {0}", intPrimeCol.Length);
+        }
+        private void btnPrimePair_Click(object sender, EventArgs e)
+        {
+            int intEven = checkBox1.Checked ? ParseSenary(txtNumber.Text) : int.Parse(txtNumber.Text);
+            if (intEven > intPrimeCol.Length)
+                return;
+            int intPrimePairCount = 0;
+            int intPICount = 0;
+            //int intPrimeLow = 0;
+          //  for (intEven = 10; intEven <= intPrimeCol.Length; intEven += 2)//for test ignore 6.8
+            {
+                StringBuilder strEvenCol = new StringBuilder();
+                intPrimePairCount = 0;
+                intPICount = 0;
+                strEvenCol.Append(checkBox1.Checked ? ToSenary(intEven) : intEven.ToString());
+                for (int i = 5; i <= (intEven >> 1); i += 2) //ignore 3 
+                {
+                    if (intPrimeCol[i] == false)
+                    {
+                        intPICount++;
+                        if (intPrimeCol[intEven - i] == false)
+                        {
+                            intPrimePairCount++;
+                            strEvenCol.Append("=");
+                            strEvenCol.Append(checkBox1.Checked ? ToSenary(i) : i.ToString());
+                            strEvenCol.Append("+");
+                            strEvenCol.Append(checkBox1.Checked ? ToSenary(intEven - i) : (intEven - i).ToString());
+
+                           /* if (i > intPrimeLow)
+                                intPrimeLow = i;
+                            break;//for test */
+                        }
+                    }
+                }
+                strEvenCol.Append("\r\n");
+                //StringBuilder strFactorCol = new StringBuilder(string.Format("pp({0})={1}, PI({2})={3}, ", intEven, intPrimePairCount, intEven >> 1, intPICount));
+                
+                StringBuilder strFactorCol = new StringBuilder(string.Format("pp({0})={1}, 2", intEven, intPrimePairCount));
+                int intTempEven = intEven >> 1;
+                for (int j = 2; j <= intTempEven;)
+                {
+                    if (intPrimeCol[j] == false && (intTempEven % j) == 0)
+                    {
+                        strFactorCol.Append("*");
+                        strFactorCol.Append(j.ToString());
+                        intTempEven /= j;
+                    }
+                    else
+                        j++;
+
+                }
+                strFactorCol.Append(";");
+                
+                if (strFactorCol.Length < 20)
+                    strFactorCol.Append("\t");
+                 richTextPrimePair.Text += strFactorCol.ToString() + strEvenCol.ToString();
+                //richTextPrimePair.Text += strFactorCol.ToString() + "\r\n";
+            }
+            txtEvenCount.Text = intPrimePairCount.ToString();          
+        }
+
+        private void btnDecide_Click(object sender, EventArgs e)
+        {
+            DecideSucceedPP();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            intNoBase = checkBox1.Checked ? 6 : 10;
+        }
+    }
+}
